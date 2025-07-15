26f283b20220879129bf96c8fc5c151bfd55a0e4
ClaudiaFuFuFu <2756470494@qq.com>
2025-07-15 09:54:08 +0800

    Delete exp.py

--- exp.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision.models as models
-
-# 加载预训练的 VGG16 模型
-vgg16 = models.vgg16(pretrained=False)
-
-# 计算总参数量
-total_params = sum(p.numel() for p in vgg16.parameters())
-print(f"VGG16 的总参数量: {total_params}")
